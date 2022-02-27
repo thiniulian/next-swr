b163485ddc7d98306770191d3f4fce0d9b1cc0ed
Iulian Gioada <iulian.gioada@thinslices.com>
2022-02-27 23:46:43 +0200

    try stale-if-error

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,7 +2,7 @@ import { date } from "../../cfg";
 export default function handler(req, res) {
   res.setHeader(
     "Cache-Control",
-    "public, s-maxage=10, stale-while-revalidate=59"
+    "public, s-maxage=10, stale-while-revalidate=59, stale-if-error=240"
   );
   const shouldError = new Date() - new Date(date) > 3 * 1000 * 60;
   res.status(shouldError ? 404 : 200).json({ name: `John Doe ${shouldError}` });

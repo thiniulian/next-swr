847bb812fc295297b86be10e6fde88c3f3310fa2
Iulian Gioada <iulian.gioada@thinslices.com>
2022-02-28 13:20:36 +0200

    again

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,16 +1,13 @@
 export default async function handler(req, res) {
-  try {
-    const data = await (
-      await fetch("https://next-swr-thiniulian.vercel.app/api/test")
-    ).json();
+  const response = await fetch(
+    "https://next-swr-thiniulian.vercel.app/api/test"
+  );
+  const data = await response.json();
 
-    res.setHeader(
-      "Cache-Control",
-      "public, s-maxage=10, stale-while-revalidate=59, stale-if-error=240"
-    );
+  res.setHeader(
+    "Cache-Control",
+    "public, s-maxage=10, stale-while-revalidate=59, stale-if-error=240"
+  );
 
-    res.status(200).json(data);
-  } catch (e) {
-    res.status(500).json({ status: "failed" });
-  }
+  res.status(response.ok ? 200 : 500).json(data);
 }
--- pages/api/test.js
+++ pages/api/test.js
@@ -1,8 +1,8 @@
-const date = "2022-02-28T11:15:00";
+const date = "2022-02-28T11:20:00";
 
 export default function handler(req, res) {
   res.setHeader("Cache-Control", "no-cache");
-  const shouldError = new Date() - new Date(date) >= 2 * 60 * 1000;
+  const shouldError = new Date() - new Date(date) >= 3 * 60 * 1000;
   res.status(shouldError ? 500 : 200).json({
     date: new Date().toISOString(),
   });
